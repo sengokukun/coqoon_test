6e7ef555c2e9e7b7517884517b5996221664d396
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-05-30 16:47:08 +0900

    refs #2 フッター作成

---
+++ src/data/footer.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    "FootList" : "Q&A"
+  },
+  {
+    "FootList" : "お問い合わせ"
+  },
+  {
+    "FootList" : "運営企業"
+  },
+  {
+    "FootList" : "利用規約"
+  },
+  {
+    "FootList" : "プライバシーポリシー"
+  }
+];
