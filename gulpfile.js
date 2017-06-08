ab4602d9f0147e90d1bd6d96117f5427641978a1
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-06-08 13:54:18 +0900

    refs #50 ヘッダーをejsでまとめる

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,10 @@ var combineMq = require('gulp-combine-mq');
 var uglify = require("gulp-uglify"); //js
 var imagemin = require("gulp-imagemin"); //img
 var imageminPngquant = require('imagemin-pngquant');
-var htmlmin = require('gulp-htmlmin'); //html圧縮
 var ejs = require("gulp-ejs");
 var rename = require("gulp-rename");
 var fileSystem = require('fs');
+var prettify = require('gulp-html-prettify');
 ////////////////////////////////////////////////////
 
 
@@ -73,30 +73,6 @@ gulp.task('imagemin', function() {
     .pipe(gulp.dest('./dist/img'));
 });
 
-// html圧縮
-gulp.task('htmlmin', function() {
-  return gulp.src('dist/**/*.html')
-  .pipe(htmlmin({collapseWhitespace: true}))
-  .pipe(gulp.dest('./dist/'))
-  .pipe(browser.reload({
-    stream: true
-  }));
-});
-
-// gulp.task("ejs", function() {
-//     var json = JSON.parse(fs.readFileSync("./src/pages.json")); //追記
-//     gulp.src(
-//        ["./src/ejs/**/*.ejs",'!' + "./src/ejs/**/_*.ejs"] //参照するディレクトリ、出力を除外するファイル
-//     )
-//     .pipe(plumber())
-//     .pipe(ejs(json)) //jsonを追記
-//     .pipe(rename({extname: ".html"})) //拡張子をhtmlに
-//     .pipe(gulp.dest("dist/")) //出力先
-//     .pipe(browser.reload({
-//       stream: true
-//     }));
-// });
-
 gulp.task('ejs', function () {
   gulp.src(
     ["./src/ejs/**/*.ejs",'!' + "./src/ejs/**/_*.ejs"]
@@ -114,15 +90,22 @@ gulp.task('ejs', function () {
     }
   ))
   .pipe(rename({extname: ".html"}))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./src'));
 });
+
+gulp.task('templates', function() {
+  gulp.src('./src/*.html')
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('./dist/'))
+});
+
 
 // default
 gulp.task("default", ["server"], function() {
   gulp.watch(['./src/scss/**/*.scss'], ['sass']);
   gulp.watch(['./src/js/**/*.js', '!./src/js/min/**/*.js'], ['uglify']);
   gulp.watch('./dist/css/**/*css',['combineMq']);
-  gulp.watch('./src/**/*.html', ['htmlmin']);
   gulp.watch('./src/img/*',['imagemin']);
   gulp.watch("./src/ejs/**/*.ejs",['ejs']);
+  gulp.watch("./src/*.html",['templates']);
 });
